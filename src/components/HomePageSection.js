dffd02cf51efd007d534132639622e8bb86fe985
Divyansh <khatri.divyansh98@gmail.com>
2019-05-10 13:45:38 +0530

    working scrollview

--- src/components/HomePageSection.js
+++ src/components/HomePageSection.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Text, View, ScrollView, Image } from 'react-native';
-import {Fonts} from '../utils/Fonts'
+import { Text, View, ScrollView, Image, TouchableOpacity } from 'react-native';
+//import {Fonts} from '../utils/Fonts'
 
 const HomePageSection = (props) => {
     let views= []
     props.item.data.map(item => {
-        views.push(<Image style = { styles.sectionStyle }  source = {{ uri: item.imageUrl, width: 200, height: 100 }} />)
+        views.push(<Image style = { styles.sectionStyle }  source = {{ uri: item.imageUrl, width: 300, height: 130 }} />)
     })
 
     return(
         <View>
-            <Text style = {styles.textStyle} >
-                {props.item.title}
-            </Text>
+            <View style = {{flexDirection: 'row'}}>
+                <Text style = {[styles.textStyle, {width: '77%' , fontSize : 19}]}  >
+                    {props.item.title}
+                </Text>
+                <View>
+                    <TouchableOpacity >
+                        <Text style = {styles.textStyle} >See all</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
             <ScrollView horizontal>
                 {views}
             </ScrollView>
@@ -22,11 +29,17 @@ const HomePageSection = (props) => {
 
 const styles = {
     sectionStyle: {
-        marginLeft: 10
+        marginLeft: 10,
+        borderRadius: 4,
+        resizeMode: 'stretch',
+        flexDirection: 'row'
+
     },
     textStyle: {
         marginTop: 20,
-        marginBottom: 20
+        marginBottom: 20,
+        fontWeight: "600",
+        marginLeft: 13
     }
 }
 export default HomePageSection;
